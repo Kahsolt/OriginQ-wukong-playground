343dc02e1ec20f6a67d28349f6ae1acfcdc2583b
Kahsolt <kahsolt@qq.com>
2024-02-06 22:01:27 +0800

    refactor translators, run again

--- run_83encoder.py
+++ run_83encoder.py
@@ -4,7 +4,6 @@
 
 # 8-3 encoder 
 
-from scipy.optimize import minimize, Bounds
 from utils import *
 
 NAME = Path(__file__).stem
@@ -20,96 +19,19 @@ dataset = [
   [1, 0, 0, 0, 0, 0, 0, 0],
 ]
 dataset = [np.asarray(x, dtype=np.float32).tolist() for x in dataset]
+dataset = [(x, x) for x in dataset]
 
 
 # Amplitude Encoder + Hardware-Efficient Ansatz
 def get_circuit(qv:List[Qubit], cv:List[CBit], X:List[float], params:List[float]) -> QProg:
-  encoder = Encode()
-  encoder.amplitude_encode(qv, X)
   prog = QProg() \
-    << encoder.get_circuit() \
-    << RX(qv[0], params[0]) \
-    << RY(qv[0], params[1]) \
-    << RX(qv[1], params[2]) \
-    << RY(qv[1], params[3]) \
-    << RX(qv[2], params[4]) \
-    << RY(qv[2], params[5]) \
-    << CNOT(qv[0], qv[1]) \
-    << CNOT(qv[1], qv[2]) \
-    << RX(qv[0], params[6]) \
-    << RY(qv[0], params[7]) \
-    << RX(qv[1], params[8]) \
-    << RY(qv[1], params[9]) \
-    << RX(qv[2], params[10]) \
-    << RY(qv[2], params[11]) \
-    << CNOT(qv[0], qv[1]) \
-    << CNOT(qv[1], qv[2]) \
-    << RX(qv[0], params[12]) \
-    << RY(qv[0], params[13]) \
-    << RX(qv[1], params[14]) \
-    << RY(qv[1], params[15]) \
-    << RX(qv[2], params[16]) \
-    << RY(qv[2], params[17]) \
+    << get_amplitude_encoder(qv, X) \
+    << get_HAE(qv, params) \
     << measure_all(qv, cv)
   return prog
 
 
-def train(maxiter=100, shot:int=3000) -> List[float]:
-  def func(params:np.ndarray) -> float:
-    nonlocal n_iter
-    n_iter += 1
-    if n_iter > maxiter: raise ValueError(f'exceed MAX_ITER: {n_iter} > {maxiter}')
-
-    losses = []
-    for X in dataset:
-      qvm, qv, cv = get_qvm_sim(3)
-      prog = get_circuit(qv, cv, X, params)
-      res = run_prog_sim(qvm, prog, shot=shot, log=False)
-      probs = np.asarray([res.get(k, 0) for k in [bin(i)[2:].rjust(3, '0') for i in range(len(dataset))]])
-      loss = ((probs - X) ** 2).mean()
-      losses.append(loss)
-    loss_avg = sum(losses) / len(losses)
-    if n_iter % 10 == 0:
-      print(f'>> [{n_iter}/{maxiter}] loss: {loss_avg}')
-    return loss_avg
-
-  n_iter = 0
-  params = np.random.uniform(low=-pi/32, high=pi/32, size=[18])
-  res = minimize(func, params, method='COBYLA', tol=1e-8, bounds=Bounds(-pi, pi), options={'maxiter': maxiter, 'disp': True})
-  print('best f(x):', res.fun)
-  print('best x:', res.x)
-  return res.x
-
-
-def infer(params:List[float], shot:int=1000, use_real:bool=False):
-  log_fp = LOG_PATH / (NAME + '.json')
-  db = load_db(log_fp)
-  try:
-    if use_real:
-      qvm, qv, cv = get_qvm(3)
-    else:
-      qvm, qv, cv = get_qvm_sim(3)
-    for X in dataset:
-      prog = get_circuit(qv, cv, X, params)
-      if use_real:
-        res = qvm.real_chip_measure(prog, shot, real_chip_type.origin_72, is_amend=False, is_mapping=False, is_optimization=False)
-      else:
-        res = run_prog_sim(qvm, prog, shot=shot)
-      probs = np.asarray([res.get(k, 0) for k in [bin(i)[2:].rjust(3, '0') for i in range(len(dataset))]])
-      print(probs)
-      db.append({
-        'X': X,
-        'res': dict(res),
-        'probs': list(probs),
-      })
-      save_db(log_fp, db)
-  except:
-    print_exc()
-  finally:
-    save_db(log_fp, db)
-
-
 # train on simulator
-params = train()
+params = train(dataset, get_circuit, maxiter=100, shot=3000)
 # infer on real chip
-infer(params, use_real=True)
+infer(dataset, get_circuit, params, NAME, shot=1000, use_real=True)
--- utils.py
+++ utils.py
@@ -10,8 +10,9 @@ from typing import *
 
 import numpy as np
 from numpy import pi
+from scipy.optimize import minimize, Bounds
 from pyqpanda import CPUQVM, QCloud, real_chip_type
-from pyqpanda import QProg, Qubit, ClassicalCondition as CBit
+from pyqpanda import QCircuit, QProg, Qubit, ClassicalCondition as CBit
 from pyqpanda import H, I, X, Y, Z, RX, RY, RZ, CR, CNOT, SWAP, Measure, measure_all
 from pyqpanda import Encode
 
@@ -73,3 +74,93 @@ def run_prog_sim(qvm:CPUQVM, prog:QProg, shot:int=1000, log:bool=True):
   res = {k: v / sum(list(res.values())) for k, v in res.items()}
   if log: print(res)
   return res
+
+
+def get_basis_encoder(qv:List[Qubit], x:List[float]) -> QCircuit:
+  encoder = Encode()
+  encoder.basic_encode(qv, x)
+  return encoder.get_circuit()
+
+def get_amplitude_encoder(qv:List[Qubit], x:List[float]) -> QCircuit:
+  encoder = Encode()
+  encoder.amplitude_encode(qv, x)
+  return encoder.get_circuit()
+
+# Hardware-Efficient Ansatz (RX-RY + CNOT)
+def get_HAE(qv:List[Qubit], params:List[float]) -> QCircuit:
+  return QCircuit() \
+    << RX(qv[0], params[0]) \
+    << RY(qv[0], params[1]) \
+    << RX(qv[1], params[2]) \
+    << RY(qv[1], params[3]) \
+    << RX(qv[2], params[4]) \
+    << RY(qv[2], params[5]) \
+    << CNOT(qv[0], qv[1]) \
+    << CNOT(qv[1], qv[2]) \
+    << RX(qv[0], params[6]) \
+    << RY(qv[0], params[7]) \
+    << RX(qv[1], params[8]) \
+    << RY(qv[1], params[9]) \
+    << RX(qv[2], params[10]) \
+    << RY(qv[2], params[11]) \
+    << CNOT(qv[0], qv[1]) \
+    << CNOT(qv[1], qv[2]) \
+    << RX(qv[0], params[12]) \
+    << RY(qv[0], params[13]) \
+    << RX(qv[1], params[14]) \
+    << RY(qv[1], params[15]) \
+    << RX(qv[2], params[16]) \
+    << RY(qv[2], params[17])
+
+
+def train(dataset:List, get_circuit:Callable, maxiter=100, shot:int=3000) -> List[float]:
+  def func(params:np.ndarray) -> float:
+    nonlocal n_iter
+    n_iter += 1
+
+    losses = []
+    for X, Y in dataset:
+      qvm, qv, cv = get_qvm_sim(3)
+      prog = get_circuit(qv, cv, X, params)
+      res = run_prog_sim(qvm, prog, shot=shot, log=False)
+      probs = np.asarray([res.get(k, 0) for k in [bin(i)[2:].rjust(3, '0') for i in range(len(dataset))]])
+      loss = ((probs - Y) ** 2).mean()
+      losses.append(loss)
+    loss_avg = sum(losses) / len(losses)
+    if n_iter % 10 == 0:
+      print(f'>> [{n_iter}/{maxiter}] loss: {loss_avg}')
+    return loss_avg
+
+  n_iter = 0
+  params = np.random.uniform(low=-pi/32, high=pi/32, size=[18])
+  res = minimize(func, params, method='COBYLA', tol=1e-8, bounds=Bounds(-pi, pi), options={'maxiter': maxiter, 'disp': True})
+  print('best f(x):', res.fun)
+  print('best x:', res.x)
+  return res.x
+
+def infer(dataset:List, get_circuit:Callable, params:List[float], name:str, shot:int=1000, use_real:bool=False):
+  log_fp = LOG_PATH / (name + '.json')
+  db = load_db(log_fp)
+  try:
+    if use_real:
+      qvm, qv, cv = get_qvm(3)
+    else:
+      qvm, qv, cv = get_qvm_sim(3)
+    for X, Y in dataset:
+      prog = get_circuit(qv, cv, X, params)
+      if use_real:
+        res = qvm.real_chip_measure(prog, shot, real_chip_type.origin_72, is_amend=False, is_mapping=False, is_optimization=False)
+      else:
+        res = run_prog_sim(qvm, prog, shot=shot)
+      probs = np.asarray([res.get(k, 0) for k in [bin(i)[2:].rjust(3, '0') for i in range(len(dataset))]])
+      print(probs)
+      db.append({
+        'X': X,
+        'res': dict(res),
+        'probs': list(probs),
+      })
+      save_db(log_fp, db)
+  except:
+    print_exc()
+  finally:
+    save_db(log_fp, db)
